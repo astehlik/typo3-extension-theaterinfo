0a97056ccfae11e7c321c997219aa5280c5df5f4
Alexander Stehlik <alexander.stehlik@googlemail.com>
2013-02-16 15:22:12 +0100

    [TASK] Fixed theaterinfo hook title

--- hooks/class.tx_theaterinfo_hooks_titles.php
+++ hooks/class.tx_theaterinfo_hooks_titles.php
@@ -1,18 +1,20 @@
 <?php
 class tx_theaterinfo_hooks_titles {
-	
-	function getActorTitle($params, $pObj) {
-		$data = t3lib_BEfunc::getRecord($params['table'], $params['row']['uid']);		
-		//Person
+
+	function getActorTitle(&$params, $pObj) {
+
+		$data = t3lib_BEfunc::getRecord($params['table'], $params['row']['uid']);
+
+			// Person
 		if($data['type'] == '0') {
 			$params['title'] = $data['lastname'] . ', ' . $data['firstname'];
 		}
-		//Company
+			// Company
 		else {
 			$params['title'] = $data['company'];
 		}
 	}
-	
+
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/theaterinfo/hooks/class.tx_theaterinfo_hooks_titles.php']) {
