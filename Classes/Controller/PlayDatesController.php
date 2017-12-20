0c88de5b84f3eb5d6a6b0e1952635b55c41abab5
Alexander Stehlik <alexander.stehlik@gmail.com>
2017-12-20 21:55:52 +0100

    [WIP] Add card order play controller and template

---
+++ Classes/Controller/PlayDatesController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Sto\Theaterinfo\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 extension "theaterinfo".              *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+/**
+ * Controller for displaying play information
+ */
+class PlayDatesController extends ActionController
+{
+   public function orderFormAction() {
+
+   }
+}
