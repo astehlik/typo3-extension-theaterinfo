fb3685f6cdd701e51080a7bac20ac668fa8a1dc1
Alexander Stehlik <alex@stehlik-online.de>
2023-05-11 15:38:49 +0200

    [TASK] Mark compatible with TYPO3 12 and PHP 8.1 and 8.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-7.4.99',
-            'typo3' => '10.4.0-10.4.99',
+            'php' => '8.1.0-8.2.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
