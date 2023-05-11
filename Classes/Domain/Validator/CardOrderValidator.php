e5f552969148bbc18be9fea77d07bd797d96b16b
Alexander Stehlik <alex@stehlik-online.de>
2023-05-11 16:04:38 +0200

    [TASK] Make CardOrderValidator compatible with TYPO3 12

--- Classes/Domain/Validator/CardOrderValidator.php
+++ Classes/Domain/Validator/CardOrderValidator.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Sto\Theaterinfo\Domain\Validator;
 
+use InvalidArgumentException;
+use Sto\Theaterinfo\Domain\Model\CardOrder;
 use TYPO3\CMS\Extbase\Validation\Error;
 use TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator;
 
@@ -11,11 +13,15 @@ class CardOrderValidator extends AbstractValidator
     /**
      * Check if $value is valid. If it is not valid, needs to add an error
      * to result.
-     *
-     * @param \Sto\Theaterinfo\Domain\Model\CardOrder $value
      */
-    protected function isValid($value)
+    protected function isValid(mixed $value): void
     {
+        if (!$value instanceof CardOrder) {
+            throw new InvalidArgumentException(
+                'The value must be an instance of \Sto\Theaterinfo\Domain\Model\CardOrder'
+            );
+        }
+
         if (!$value->hasOrderedCards()) {
             $this->result->forProperty('rows')->addError(new Error('No cards orderd.', 1520190410));
         }
