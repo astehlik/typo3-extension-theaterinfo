c36c2742a9d40ab8ca9c543ffc0583e136871dea
Alexander Stehlik <alex@stehlik-online.de>
2024-11-05 17:22:47 +0100

    [TASK] Cleanup / update ext_emconf.php
    Remove obsolete entries, update dependencies, reorder based on
    documentation

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,25 +1,19 @@
 <?php
 
-/** @noinspection PhpMissingStrictTypesDeclarationInspection */
+/** @var string $_EXTKEY */
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Theater info',
     'description' => 'Displays information about theater plays, actors and their roles and tasks',
+    'version' => '13.4.0-dev',
     'category' => 'plugin',
-    'state' => 'alpha',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'author' => 'Alexander Stehlik',
-    'author_email' => 'alexander.stehlik.deleteme@googlemail.com',
-    'author_company' => '',
-    'version' => '1.0.0',
     'constraints' => [
-        'depends' => [
-            'php' => '8.1.0-8.2.99',
-            'typo3' => '12.4.0-12.4.99',
-        ],
+        'depends' => ['typo3' => '13.4.0-13.4.99'],
         'conflicts' => [],
         'suggests' => [],
     ],
+    'state' => 'beta',
+    'author' => 'Alexander Stehlik',
+    'author_email' => 'alexander.stehlik.deleteme@gmail.com',
+    'author_company' => '',
 ];
