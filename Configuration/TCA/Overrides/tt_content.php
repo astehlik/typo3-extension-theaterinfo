61c33aaec0710ef7c432d501d3e4a7a3fdeaf6f9
Alexander Stehlik <alex@stehlik-online.de>
2024-11-05 18:15:21 +0100

    [TASK] Improve plugin registration
    Add translations for plugin labels and description, add icons,
    move to dedicated theaterinfo group.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,25 +2,61 @@
 
 declare(strict_types=1);
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-ExtensionUtility::registerPlugin(
-    'Theaterinfo',
-    'PlaysList',
-    'Show plays (Theater info)',
-    group: 'theaterinfo',
-);
+defined('TYPO3') || exit;
 
-ExtensionUtility::registerPlugin(
-    'Theaterinfo',
-    'ShowManagement',
-    'Show management (Theater info)',
-    group: 'theaterinfo',
-);
+call_user_func(static function (): void {
+    $lllPrefxCtype = 'LLL:EXT:theaterinfo/Resources/Private/Language/locallang_db.xlf:tt_content.CType.';
 
-ExtensionUtility::registerPlugin(
-    'Theaterinfo',
-    'CardOrderForm',
-    'Card order (Theater info)',
-    group: 'theaterinfo',
-);
+    ExtensionManagementUtility::addTcaSelectItemGroup(
+        'tt_content',
+        'CType',
+        'theaterinfo',
+        $lllPrefxCtype . 'group.theaterinfo',
+    );
+
+    $enablePagesForContentType = static function (string $contentTypeName): void {
+        ExtensionManagementUtility::addToAllTCAtypes(
+            'tt_content',
+            '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:plugin, pages',
+            $contentTypeName,
+            'after:palette:headers',
+        );
+    };
+
+    $contentTypeName = ExtensionUtility::registerPlugin(
+        'Theaterinfo',
+        'PlaysList',
+        $lllPrefxCtype . 'theaterinfo_playslist',
+        'content-inside-text-img-right',
+        'theaterinfo',
+        $lllPrefxCtype . 'theaterinfo_playslist.description',
+    );
+
+    $enablePagesForContentType($contentTypeName);
+
+
+    $contentTypeName = ExtensionUtility::registerPlugin(
+        'Theaterinfo',
+        'ShowManagement',
+        $lllPrefxCtype . 'theaterinfo_showmanagement',
+        'content-briefcase',
+        'theaterinfo',
+        $lllPrefxCtype . 'theaterinfo_showmanagement.description',
+    );
+
+    $enablePagesForContentType($contentTypeName);
+
+    $contentTypeName = ExtensionUtility::registerPlugin(
+        'Theaterinfo',
+        'CardOrderForm',
+        $lllPrefxCtype . 'theaterinfo_cardorderform',
+        'content-book',
+        'theaterinfo',
+        $lllPrefxCtype . 'theaterinfo_cardorderform.description',
+    );
+
+    $enablePagesForContentType($contentTypeName);
+});
